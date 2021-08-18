4bded65cf570ad1ba9544002274bf2c93c088938
unknown <mobinh264@gmail.com>
2021-08-18 13:49:46 +0600

    Varify match and use Function To reduce duplicate

--- js/app.js
+++ js/app.js
@@ -12,25 +12,49 @@ function generatePin(){
   
     const pin = getPin()
     const displayPin = document.getElementById('display-pin');
-    displayPin.value =  getPin();
+    displayPin.value =  getPin(pin);
 }
 
 // Using work in Calculator
 
 document.getElementById('key-pad').addEventListener('click',function(event){
     const number = event.target.innerText;
-    if( isNaN(number) ){
-        alert("Please give a Valid input");
-    }else{
-   
     const calcInput = document.getElementById('typed-numbers');
+
+    if( isNaN(number) ){
+        if( number == 'C' ){
+            calcInput.value = '';
+        }else if( number == '<' ){
+            // calcInput.value = indexOf(calcInput).length - 1;
+            calcInput.value = calcInput.value.substring( 0, calcInput.value.length - 1);
+            // calcInput.value = calcInput.value.length - 1;
+        }
+        // else{
+        //     alert("Please give a Valid input");
+        // }
+    }
+    else{
     const previousNumber = calcInput.value;
     const newNumber = previousNumber + number;
     calcInput.value = newNumber;
 
     }
-//    const previusNumber =  typedNumber.value  = event.target.innerText;
-//    const currentNumber = previusNumber + 
 
+});
 
-})
+function verifyPin(){
+    const displayPin = document.getElementById('display-pin');
+    const typedNumber = document.getElementById('typed-numbers');
+   const notifyFaild =  document.getElementById('notify-fail');
+   const notifySuccess =  document.getElementById('notify-success');
+
+    if( displayPin.value === typedNumber.value ){
+
+        notifySuccess.style.display = 'block';
+        notifyFaild.style.display = 'none';
+
+    }else{
+        notifySuccess.style.display = 'none';
+        notifyFaild.style.display = 'block';
+    }
+}
