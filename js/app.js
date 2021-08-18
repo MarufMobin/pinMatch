af7678690b511eabfa1caca46e5e343e5c026a13
unknown <mobinh264@gmail.com>
2021-08-18 06:18:58 +0600

    Fetch any number in calculator display

--- js/app.js
+++ js/app.js
@@ -14,3 +14,23 @@ function generatePin(){
     const displayPin = document.getElementById('display-pin');
     displayPin.value =  getPin();
 }
+
+// Using work in Calculator
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    if( isNaN(number) ){
+        alert("Please give a Valid input");
+    }else{
+   
+    const calcInput = document.getElementById('typed-numbers');
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+
+    }
+//    const previusNumber =  typedNumber.value  = event.target.innerText;
+//    const currentNumber = previusNumber + 
+
+
+})
